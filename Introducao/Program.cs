8505222eaf608069e84573c8659f35c7e12fdac2
RhyanSKomm <rhyankomm@hotmail.com>
2025-03-24 13:27:54 -0400

    first commit

---
+++ Introducao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Teste
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Builder = WebApplication.CreateBuilder(args);
+            var app = Builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapGet("/", async context => {
+                    context.Response.Redirect("/index.html");
+                });
+            });
+
+            app.Run();
+        }
+
+        // public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //     Host.CreateDefaultBuilder(args)
+        //         .ConfigureWebHostDefaults(webBuilder =>
+        //         {
+        //             webBuilder.UseStartup<Startup>();
+        //         });
+    }
+}
